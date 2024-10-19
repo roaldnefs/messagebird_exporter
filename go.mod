0fb8eb1a9bab42dc78e5dc5a8b63a1abf9055e9b
Roald Nefs <info@roaldnefs.com>
2024-10-19 13:04:31 +0200

    feat: use generic prometheus components
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- go.mod
+++ go.mod
@@ -1,11 +1,37 @@
 module github.com/roaldnefs/messagebird_exporter
 
-go 1.15
+go 1.22
+
+toolchain go1.22.5
 
 require (
+	github.com/alecthomas/kingpin/v2 v2.4.0
 	github.com/messagebird/go-rest-api v5.3.0+incompatible
-	github.com/prometheus/client_golang v1.9.0
-	github.com/prometheus/common v0.15.0
-	github.com/sirupsen/logrus v1.7.0
-	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	github.com/prometheus/client_golang v1.20.4
+	github.com/prometheus/common v0.60.0
+	github.com/prometheus/exporter-toolkit v0.13.0
+)
+
+require (
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/mdlayher/socket v0.4.1 // indirect
+	github.com/mdlayher/vsock v1.2.1 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/mwitkow/go-conntrack v0.0.0-20190716064945-2f068394615f // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/oauth2 v0.23.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- messagebird_exporter.go
+++ messagebird_exporter.go
@@ -1,43 +1,55 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 	"time"
 
 	messagebird "github.com/messagebird/go-rest-api"
 	"github.com/messagebird/go-rest-api/balance"
 
+	"github.com/alecthomas/kingpin/v2"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/exporter-toolkit/web"
+	webflag "github.com/prometheus/exporter-toolkit/web/kingpinflag"
 
+	"github.com/prometheus/common/promslog"
 	"github.com/prometheus/common/version"
-
-	"gopkg.in/alecthomas/kingpin.v2"
-
-	log "github.com/sirupsen/logrus"
 )
 
 var (
+	metricsPath = kingpin.Flag(
+		"web.telemetry-path",
+		"Path under which to expose metrics.",
+	).Default("/metrics").String()
+	toolkitFlags = webflag.AddFlags(kingpin.CommandLine, ":9601")
+	apiKey       = kingpin.Flag(
+		"messagebird.api-key",
+		"MessageBird Api Key.",
+	).Required().String()
+
 	credits = promauto.NewGauge(prometheus.GaugeOpts{
 		Name: "messagebird_balance_credits",
 		Help: "The total number of credits remaining in the MessageBird account",
 	})
 
 	client *messagebird.Client
+	logger *slog.Logger
 )
 
 func recordMetrics() {
 	go func() {
 		for {
-			// Request the balance information
+			// Request the balance information.
 			balance, err := balance.Read(client)
 			if err != nil {
 				switch errResp := err.(type) {
 				case messagebird.ErrorResponse:
-					for _, mbError := range errResp.Errors {
-						log.Error(fmt.Sprintf("%#v", mbError))
+					for _, err := range errResp.Errors {
+						logger.Error("Error retrieving balance from the MessageBird account", "err", err)
 					}
 				}
 			} else {
@@ -47,60 +59,51 @@ func recordMetrics() {
 			time.Sleep(10 * time.Second)
 		}
 	}()
-
 }
 
 func main() {
-	var (
-		listenAddress = kingpin.Flag(
-			"web.listen-address",
-			"Address on which to expose metrics and web interface.",
-		).Default(":9601").String()
-		metricsPath = kingpin.Flag(
-			"web.telemetry-path",
-			"Path under which to expose metrics.",
-		).Default("/metrics").String()
-		apiKey = kingpin.Flag(
-			"messagebird.api-key",
-			"MessageBird Api Key.",
-		).Required().String()
-	)
-
+	promslogConfig := &promslog.Config{}
 	kingpin.Version(version.Print("messagebird_exporter"))
-
 	kingpin.HelpFlag.Short('h')
 	kingpin.Parse()
+	logger = promslog.New(promslogConfig)
 
-	// Initialize a new MessageBird client.
+	// Initialize a new MessageBird client using the provided API key.
 	client = messagebird.New(*apiKey)
 
-	registry := prometheus.NewRegistry()
-	registry.MustRegister(credits)
-	registry.MustRegister(version.NewCollector("messagebird_exporter"))
-	handler := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
-
-	log.WithFields(log.Fields{
-		"version": version.Version,
-	}).Info("Starting MessageBird Exporter")
+	logger.Info("Starting messagebird_exporter", "version", version.Info())
+	logger.Info("operational information", "build_context", version.BuildContext())
 
 	recordMetrics()
 
-	http.Handle(*metricsPath, handler)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`<html>
-				<head><title>MessageBird Exporter</title></head>
-				<body>
-				<h1>MessageBird Exporter</h1>
-				<p><a href="` + *metricsPath + `">Metrics</a></p>
-				</body>
-				</html>`))
-	})
+	// Define the metrics page based on the metrics path.
+	http.Handle(*metricsPath, promhttp.Handler())
 
-	log.WithFields(log.Fields{
-		"listen_address": *listenAddress,
-		"metrics_path":   *metricsPath,
-	}).Info("Listening on " + *listenAddress)
-	if err := http.ListenAndServe(*listenAddress, nil); err != nil {
-		log.Fatal(err)
+	// Define the landing page if the metrics path is not set to the root.
+	if *metricsPath != "/" {
+		landingConfig := web.LandingConfig{
+			Name:        "MessageBird Exporter",
+			Description: "MessageBird Exporter",
+			Version:     version.Info(),
+			Links: []web.LandingLinks{
+				{
+					Address: *metricsPath,
+					Text:    "Metrics",
+				},
+			},
+		}
+		landingPage, err := web.NewLandingPage(landingConfig)
+		if err != nil {
+			logger.Error(err.Error())
+			os.Exit(1)
+		}
+		http.Handle("/", landingPage)
+	}
+
+	// Start the HTTP server.
+	srv := &http.Server{}
+	if err := web.ListenAndServe(srv, toolkitFlags, logger); err != nil {
+		logger.Error("Error starting HTTP server", "err", err)
+		os.Exit(1)
 	}
 }
